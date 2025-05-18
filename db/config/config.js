f6a91d3338f870273fb090e476a75c4db420cd11
AbiskarLc10 <abiskarlamichhane10@gmail.com>
2025-05-18 19:56:17 +0545

    Changes

--- db/config/config.js
+++ db/config/config.js
@@ -1,26 +1,23 @@
-module.exports = {
-  development: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'newdb',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 3306,
-    dialect: process.env.DB_DIALECT || 'mysql',
+module.exports={
+  "development": {
+    "username": "root",
+    "password": "password",
+    "database": "testdb",
+    "host": "mysqldb",
+    "dialect": "mysql"
   },
-  test: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'newdb',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 3306,
-    dialect: process.env.DB_DIALECT || 'mysql',
+  "test": {
+    "username": "root",
+    "password": "password",
+    "database": "newdb",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
-  production: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME || 'database_production',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 3306,
-    dialect: process.env.DB_DIALECT || 'mysql',
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   }
-};
+}
